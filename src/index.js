a5a0c8108562bc6b9f32bbbf47159b6c52ee8b50
pradhuman singh rathore <pradhumansinghrathore55@gmail.com>
2020-07-08 15:52:32 +0530

    navbar

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Nav from './Pages/Nav';
 import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Nav />
   </React.StrictMode>,
   document.getElementById('root')
 );
