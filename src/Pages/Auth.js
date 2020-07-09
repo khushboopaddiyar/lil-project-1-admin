e77d9cf1503c4ca90446bd22570f0adeeb17773c
“[khushboo <khushboopaddiyar@gmail.com>
2020-07-09 10:45:36 +0530

    login page

--- src/Pages/Auth.js
+++ src/Pages/Auth.js
@@ -1,119 +1,98 @@
-import React, { useState, useEffect } from 'react';
-import TextField from '@material-ui/core/TextField';
-import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import CardActions from '@material-ui/core/CardActions';
+import React from 'react';
+import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
-import CardHeader from '@material-ui/core/CardHeader';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
 
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    container: {
-      display: 'flex',
-      flexWrap: 'wrap',
-      width: 400,
-      margin: `${theme.spacing(0)} auto`
-    },
-    loginBtn: {
-      marginTop: theme.spacing(2),
-      flexGrow: 1,
-      background: "#306EFF"
-    },
-    header: {
-      textAlign: 'center',
-      background: '#306EFF',
-      color: '#fff'
-    },
-    card: {
-      marginTop: theme.spacing(10)
-    }
 
-  }),
-);
 
-const Login = () => {
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+export default function SignIn() {
   const classes = useStyles();
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [isButtonDisabled, setIsButtonDisabled] = useState(true);
-  const [helperText, setHelperText] = useState('');
-  const [error, setError] = useState(false);
-
-  useEffect(() => {
-    if (username.trim() && password.trim()) {
-      setIsButtonDisabled(false);
-    } else {
-      setIsButtonDisabled(true);
-    }
-  }, [username, password]);
-
-  const handleLogin = () => {
-    if (username === 'abc@email.com' && password === 'password') {
-      setError(false);
-      setHelperText('Login Successfully');
-    } else {
-      setError(true);
-      setHelperText('Incorrect username or password')
-    }
-  };
-
-  const handleKeyPress = (e:any) => {
-    if (e.keyCode === 13 || e.which === 13) {
-      isButtonDisabled || handleLogin();
-    }
-  };
 
   return (
-    <React.Fragment>
-      <form className={classes.container} noValidate autoComplete="off">
-        <Card className={classes.card}>
-          <CardHeader className={classes.header} title="Login App" />
-          <CardContent>
-            <div>
-              <TextField
-                error={error}
-                fullWidth
-                id="username"
-                type="email"
-                label="Username"
-                placeholder="Username"
-                margin="normal"
-                onChange={(e)=>setUsername(e.target.value)}
-                onKeyPress={(e)=>handleKeyPress(e)}
-              />
-              <TextField
-                error={error}
-                fullWidth
-                id="password"
-                type="password"
-                label="Password"
-                placeholder="Password"
-                margin="normal"
-                helperText={helperText}
-                onChange={(e)=>setPassword(e.target.value)}
-                onKeyPress={(e)=>handleKeyPress(e)}
-              />
-            </div>
-          </CardContent>
-          <CardActions>
-            <Button
-              variant="contained"
-              size="large"
-              color="secondary"
-              className={classes.loginBtn}
-              onClick={()=>handleLogin()}
-              disabled={isButtonDisabled}>
-              Login
-            </Button>
-          </CardActions>
-        </Card>
-      </form>
-    </React.Fragment>
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <form className={classes.form} noValidate>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+          
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Sign In
+          </Button>
+          <Grid container>
+            <Grid item xs>
+              <Link href="#" variant="body2">
+                Forgot password?
+              </Link>
+            </Grid>
+            
+          </Grid>
+        </form>
+      </div>
+      
+    </Container>
   );
 }
-
-export default Login;
-
-    
 
--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import * as serviceWorker from './serviceWorker';
 ReactDOM.render(
 
     <Form />,
+    
   document.getElementById('root')
 );
 
